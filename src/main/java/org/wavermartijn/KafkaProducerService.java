7637ce1ccdb75cb609429402228084979c0fc0ec
Martijn van der Pauw <martijn.vanderpauw@meteogroup.com>
2016-10-14 14:24:29 +0200

    move intitalization of producer to postconstruct

--- src/main/java/org/wavermartijn/KafkaProducerService.java
+++ src/main/java/org/wavermartijn/KafkaProducerService.java
@@ -7,6 +7,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -19,31 +20,17 @@ public class KafkaProducerService {
   Producer<String, String> producer = null;
   KafkaConsumer consumer = null;
 
-  public KafkaProducerService() {
 
+  @PostConstruct
+  public void intitalizeKafkaProducer() throws IOException {
+    props = new Properties();
+    props.load(KafkaProducerService.class.getResourceAsStream("/producer.props"));
+    producer = new KafkaProducer<>(props);
   }
 
   public void produceMessage() throws IOException {
-    if (props == null) {
 
-      props = new Properties();
-      props.load(KafkaProducerService.class.getResourceAsStream("/producer.props"));
-      //      props.put("zookeeper.connect","172.17.0.1:2181");
-      //      props.put("bootstrap.servers", "172.17.0.1:9092");
-      //
-      //      props.put("acks", "all");
-      //      props.put("retries", 1);
-      //      props.put("auto.commit.interval.ms", "1000");
-      //      props.put("batch.size", 10);
-      //      props.put("linger.ms", 5);
-      //      props.put("buffer.memory", 33554432);
-      //      props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      //      props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-      producer = new KafkaProducer<>(props);
-
-    }
-    log.info("going to send");
+    log.info("going to send some test messages");
     for (int i = 0; i < 100; i++) {
       producer.send(new ProducerRecord<String, String>("martijn1", Integer.toString(i), "martijn is " + i));
     }
